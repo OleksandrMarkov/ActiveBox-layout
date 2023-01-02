f37f06ac8f030c292f80fbbcbce9b5b85132b722
Oleksandr Markov <ol.markov1999@gmail.com>
2023-01-02 17:07:23 +0200

    init main window

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+class HotelManagementSystem:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Hotel Management System")
+        self.root.geometry("1550x800+0+0")
+
+
+
+if __name__ == '__main__':
+    root = Tk()
+    project = HotelManagementSystem(root)
+    root.mainloop()
