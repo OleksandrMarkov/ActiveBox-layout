a633506d55d53a1578688d60762676a8c1aebbbc
Oleksandr Markov <ol.markov1999@gmail.com>
2023-01-02 19:33:36 +0200

    menu and images on main window

--- main.py
+++ main.py
@@ -26,10 +26,75 @@ class HotelManagementSystem:
         lbling.place(x=0, y=0, width = 230, height = 140)
 
         #####################Title##############################
-        lbl_title = Label(self.root, text = "Hotel Management System",\
+        lbl_title = Label(self.root, text = "Hotel Management System".upper(),\
         font = ("times new roman", 40, "bold"), bg = "black", fg = "gold",\
         bd = 4, relief = RIDGE)
         lbl_title.place(x = 0, y = 140, width = 1550, height = 50)
+
+        #####################Main frame##############################
+        main_frame = Frame(self.root, bd = 4, relief = RIDGE)
+        main_frame.place(x = 0, y = 190, width = 1550, height = 620)
+
+        #####################Menu##############################
+        lbl_menu = Label(main_frame, text="MENU", \
+        font=("times new roman", 20, "bold"), bg="black", fg="gold", \
+        bd=4, relief=RIDGE)
+        lbl_menu.place(x = 0, y = 0, width= 230)
+
+        #####################Buttons frame##############################
+        btn_frame = Frame(main_frame, bd=4, relief=RIDGE)
+        btn_frame.place(x = 0, y = 35, width = 228, height = 190)
+
+        cust_btn = Button(btn_frame, text = "CUSTOMER", width = 22, \
+        font=("times new roman", 14, "bold"), bg="black", fg="gold",\
+        bd = 0, cursor = 'hand1')
+        cust_btn.grid(row = 0, column = 0, pady = 1)
+
+        room_btn = Button(btn_frame, text="ROOM", width=22, \
+        font=("times new roman", 14, "bold"), bg="black", fg="gold", \
+        bd=0, cursor='hand1')
+        room_btn.grid(row = 1, column = 0, pady = 1)
+
+        details_btn = Button(btn_frame, text="DETAILS", width=22, \
+        font=("times new roman", 14, "bold"), bg="black", fg="gold", \
+        bd=0, cursor='hand1')
+        details_btn.grid(row = 2, column = 0, pady = 1)
+
+        report_btn = Button(btn_frame, text="REPORT", width=22, \
+        font=("times new roman", 14, "bold"), bg="black", fg="gold", \
+        bd=0, cursor='hand1')
+        report_btn.grid(row = 3, column = 0, pady = 1)
+
+        logout_btn = Button(btn_frame, text="LOGOUT", width=22, \
+        font=("times new roman", 14, "bold"), bg="black", fg="gold", \
+        bd=0, cursor='hand1')
+        logout_btn.grid(row = 4, column = 0, pady = 1)
+
+        #####################Right side image##############################
+        img3 = Image.open("images\slide3.jpg")
+        img3 = img3.resize((1310, 590), Image.ANTIALIAS)
+        self.photoimg3 = ImageTk.PhotoImage(img3)
+
+        lbling1 = Label(main_frame, image = self.photoimg3, bd = 4, \
+        relief = RIDGE)
+        lbling1.place(x = 225, y = 0, width = 1310, height = 590)
+
+        #####################Down images##############################
+        img4 = Image.open("images\myh.jpg")
+        img4 = img4.resize((230, 210), Image.ANTIALIAS)
+        self.photoimg4 = ImageTk.PhotoImage(img4)
+
+        lbling1 = Label(main_frame, image=self.photoimg4, bd=4, \
+                        relief=RIDGE)
+        lbling1.place(x = 0, y = 225, width = 230, height = 210)
+
+        img5 = Image.open("images\khana.jpg")
+        img5 = img5.resize((230, 190), Image.ANTIALIAS)
+        self.photoimg5 = ImageTk.PhotoImage(img5)
+
+        lbling1 = Label(main_frame, image=self.photoimg5, bd=4, \
+        relief=RIDGE)
+        lbling1.place(x = 0, y = 420, width = 230, height = 190)
 
 if __name__ == '__main__':
     root = Tk()
